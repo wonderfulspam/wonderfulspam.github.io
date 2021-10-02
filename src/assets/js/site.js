52061dd3f6604c0630e23364602394bdfffb16ab
Emil Thorenfeldt <emt@magenta-aps.dk>
2021-10-03 00:07:21 +0200

    Minor cleanup and refactor

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,11 +1,11 @@
-addArtistClickHandler = function(linkClassname, targetId) {
+addCopyClickHandler = function(linkClassname, targetId) {
     let links = document.querySelectorAll(linkClassname);
     for(var i = 0; i < links.length; i++){
         links[i].addEventListener('click', function(event) {
             event.preventDefault();
             let slug = this.dataset.slug;
-            let artistHtml = document.getElementById(slug).innerHTML;
-            document.getElementById(targetId).innerHTML = artistHtml;
+            let targetHtml = document.getElementById(slug).innerHTML;
+            document.getElementById(targetId).innerHTML = targetHtml;
         }, false);
     }
 }
